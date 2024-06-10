fe64a1e05adfc5fd6c5384f9278c0869eb2d96ed
Inze Bielderman <Bielderman@InzeNL.com>
2024-06-10 21:33:00 +0200

    Fixes compilation error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, redirect, render_template, jsonify, request
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap5
 from wtforms import StringField, IntegerField, SubmitField
